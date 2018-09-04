a28cfd11c4ac5f960bd58c50e418aa1b46972e37
Bradley Morris <bradleymorrisza@gmail.com>
2018-09-04 23:41:00 +0200

    added cross origins for local testing

--- src/main/java/kalaha/controllers/GameController.java
+++ src/main/java/kalaha/controllers/GameController.java
@@ -19,14 +19,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost", maxAge=3600)
 @Controller
 @RequestMapping("/game")
 public class GameController {
